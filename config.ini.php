92d8029886c2931e9277ad2c22ea3023d654c2fb
TheNotary <no@email.plz>
2016-09-17 19:19:43 -0500

    allows reading db configuration from DATABASE_URL env var

--- config.ini.php
+++ config.ini.php
@@ -1,12 +1,33 @@
 ; <?php exit; ?> DO NOT REMOVE THIS LINE
 ; file automatically generated or modified by Piwik; you can manually override the default values in global.ini.php by redefining them in this file.
+<?php 
+  // This will parse the values from DATABASE_URL if present (true on dokku, 
+  // once you link a database to your app
+  if ($_ENV['DATABASE_URL'] != null) {
+    $u = parse_url($_ENV['DATABASE_URL']);
+
+    $db_host = $u['host'];
+    $db_user= $u['user'];
+    $db_pass= $u['pass'];
+    $db_name = $u['path'];
+    $db_port = $u['port'];
+  }
+  else {
+    $db_host = $_ENV['DB_HOST'];
+    $db_user= $_ENV['DB_USERNAME'];
+    $db_pass= $_ENV['DB_PASSWORD'];
+    $db_name = $_ENV['DB_NAME'];
+    $db_port = $_ENV['DB__PORT'];
+  }
+?>
+
 [database]
-host = <?php $_ENV['DB_HOST']; ?>
-username = <?php $_ENV['DB_USERNAME']; ?>
-password = <?php $_ENV['DB_PASSWORD']; ?>
-dbname = <?php $_ENV['DB_NAME']; ?>
+host = <?php $db_host; ?>
+username = <?php $db_user; ?>
+password = <?php $db_pass; ?>
+dbname = <?php $db_name; ?>
 tables_prefix = <?php $_ENV['DB_PREFIX']; ?>
-port = <?php $_ENV['DB__PORT']; ?>
+port = <?php $db_port; ?>
 charset = "utf8"
 
 [General]
