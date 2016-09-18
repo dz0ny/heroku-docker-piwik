ed6e3af69e62c18e35cba2fd7f3891bcfe81754d
TheNotary <no@email.plz>
2016-09-18 00:37:18 -0500

    using ENV[name] syntax

--- config.ini.php
+++ config.ini.php
@@ -1,37 +1,48 @@
 ; <?php exit; ?> DO NOT REMOVE THIS LINE
 ; file automatically generated or modified by Piwik; you can manually override the default values in global.ini.php by redefining them in this file.
-<?php 
-  // This will parse the values from DATABASE_URL if present (true on dokku, 
-  // once you link a database to your app
-  if ($_ENV['DATABASE_URL'] != null) {
-    $u = parse_url($_ENV['DATABASE_URL']);
+;<?php 
+;  // This will parse the values from DATABASE_URL if present (true on dokku, 
+;  // once you link a database to your app
+;  if ($_ENV['DATABASE_URL'] != null) {
+;    $u = parse_url($_ENV['DATABASE_URL']);
+;
+;    $db_host = $u['host'];
+;    $db_user= $u['user'];
+;    $db_pass= $u['pass'];
+;    $db_name = $u['path'];
+;    $db_port = $u['port'];
+;  }
+;  else {
+;    $db_host = $_ENV['DB_HOST'];
+;    $db_user= $_ENV['DB_USERNAME'];
+;    $db_pass= $_ENV['DB_PASSWORD'];
+;    $db_name = $_ENV['DB_NAME'];
+;    $db_port = $_ENV['DB__PORT'];
+;  }
+;?>
+;
+;[database]
+;host = "<?php echo $db_host; ?>"
+;username = "<?php echo $db_user; ?>"
+;password = "<?php echo $db_pass; ?>"
+;dbname = "<?php echo $db_name; ?>"
+;tables_prefix = "<?php echo $_ENV['DB_PREFIX']; ?>"
+;port = "<?php echo $db_port; ?>"
+;charset = "utf8"
 
-    $db_host = $u['host'];
-    $db_user= $u['user'];
-    $db_pass= $u['pass'];
-    $db_name = $u['path'];
-    $db_port = $u['port'];
-  }
-  else {
-    $db_host = $_ENV['DB_HOST'];
-    $db_user= $_ENV['DB_USERNAME'];
-    $db_pass= $_ENV['DB_PASSWORD'];
-    $db_name = $_ENV['DB_NAME'];
-    $db_port = $_ENV['DB__PORT'];
-  }
-?>
+;mysql:b57155eb5cd509af@dokku-mysql-piwik-db:3306/piwik-db
 
 [database]
-host = "<?php echo $db_host; ?>"
-username = "<?php echo $db_user; ?>"
-password = "<?php echo $db_pass; ?>"
-dbname = "<?php echo $db_name; ?>"
-tables_prefix = "<?php echo $_ENV['DB_PREFIX']; ?>"
-port = "<?php echo $db_port; ?>"
+host = dokku-mysql-piwik-db
+username = ENV[DB_USERNAME]
+password = ENV[DB_PASSWORD]
+dbname = ENV[DB_NAME]
+tables_prefix = 
+port = ENV[DB_PORT]
 charset = "utf8"
 
 [General]
-salt = "<?php $_ENV['SECRET'] ?>"
+salt = cidjdkfjldcn
 session_save_handler = dbtable
 
 [PluginsInstalled]
